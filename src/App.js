dbaddf084490e1a5313a03a0083793010a8db9e2
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2021-03-19 15:28:25 +0100

    Initial commit and draw grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import produce from 'immer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const numRows = 50
+const numCols = 50
+
+const App: React.FC = () => {
+    const [grid, setGrid] = useState(() => {
+        const rows = []
+
+        for (let i = 0; i < numRows; i++) {
+            rows.push(Array.from(Array(numCols), () => 0))
+        }
+
+        return rows
+    })
+
+    return <>
+        <div style={{
+            display: 'grid',
+            gridTemplateColumns: `repeat(${numCols}, 20px)`
+        }}>
+            {grid.map((rows, i) =>
+                rows.map((col, j) => (
+                    <div
+                        key={`${i}-${j}`}
+                        onClick={() => {
+                            const newGrid = produce(grid, gridCopy => {
+                                gridCopy[i][j] = !grid[i][j]
+                            })
+
+                            setGrid(newGrid)
+                        }}
+                        style={{
+                            backgroundColor: grid[i][j] ? 'pink' : 'white',
+                            border: '1px solid lightgrey',
+                            height: 20,
+                            width: 20
+                        }}
+                    />
+                ))
+            )}
+        </div>
+    </>
 }
 
-export default App;
+export default App
